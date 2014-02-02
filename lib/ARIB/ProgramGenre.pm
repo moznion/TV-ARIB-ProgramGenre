054ea79104d9b4b5cd6afeb686cca0dad7055ebb
moznion <moznion@gmail.com>
2014-02-03 01:27:23 +0900

    Initial commit

---
+++ lib/ARIB/ProgramGenre.pm
@@ -0,0 +1,39 @@
+package ARIB::ProgramGenre;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+ARIB::ProgramGenre - It's new $module
+
+=head1 SYNOPSIS
+
+    use ARIB::ProgramGenre;
+
+=head1 DESCRIPTION
+
+ARIB::ProgramGenre is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    ARIB::ProgramGenre
+);
+
+done_testing;
+
